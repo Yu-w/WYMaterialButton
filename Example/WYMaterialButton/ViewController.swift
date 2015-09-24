46e59a5fbbfd223fdbf4e8b13c22b5121b54242f
Yu Wang <wangyu2165@gmail.com>
2015-09-24 16:37:26 -0500

    Initial commit

---
+++ Example/WYMaterialButton/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  WYMaterialButton
+//
+//  Created by Yu Wang on 09/24/2015.
+//  Copyright (c) 2015 Yu Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
